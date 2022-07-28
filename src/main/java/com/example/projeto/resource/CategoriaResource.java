be45904dca4481d1e91d52cea8ebd87f354b70e6
igorthiago5 <projetoSalomao>
2022-07-27 20:28:51 -0400

    Contrução da classe Categoria

--- src/main/java/com/example/projeto/resource/CategoriaResource.java
+++ src/main/java/com/example/projeto/resource/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.example.projeto.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.projeto.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categoria")
@@ -10,7 +15,14 @@ public class CategoriaResource {
 	
 	//devido ao uso simples do uso, queremo só 
 	@RequestMapping(method=RequestMethod.GET)
-	public String lista() {
-		return "Rest ok";
+	public List<Categoria> lista() {
+		Categoria categoria = new Categoria(1,"informatica");
+		Categoria categoria2 = new Categoria(1,"Escritório");
+		
+		List<Categoria> c1 = new ArrayList<>();
+		c1.add(categoria);
+		c1.add(categoria2);
+		
+		return c1;
 	}
 }
